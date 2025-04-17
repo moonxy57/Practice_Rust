6ddcee30742f50dce7d69e5e7cac9b3924ef7d4f
moonxy57 <c1208023@st.kanazawa-it.ac.jp>
2025-04-17 22:03:46 +0900

    型を学んで練習した

---
+++ beginner/const.rs
@@ -0,0 +1,8 @@
+const PI: f64 = 3.141592653589793238;
+
+fn main() {
+    println!(
+        "Hello World!",
+        PI
+    );
+}
---
+++ beginner/number_type2.rs
@@ -0,0 +1,8 @@
+const z: f64 = 84273281.0;
+
+fn main(){
+    println!(
+        "Bye!!",
+        z
+    );
+}
